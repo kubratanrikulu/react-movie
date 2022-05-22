837c20c7cfd3b51860baeb2a7f6975d3f3170365
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2022-05-22 11:48:17 +0300

    link tmdb api

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -5,15 +5,18 @@ const MovieList = (props) => {
     return (
         <div className='row'>
             {props.movies.map((movie) => (
-                <div className='col-lg-4 key' key={movie.id}>
-                    <div className='card mb-4 shadow-sm'>
-                        <img src={movie.imageURL} className='card-img-top' />
+                <div className='col-lg-3 key' key={movie.id}>
+                    <div className='card mb-4 shadow-sm bg-black'>
+                        <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} className='card-img-top w-75 d-flex jutfiy-content-center' />
+                        <div className='content position-relative d-flex'>
+                        <div className='badge position-absolute'><h4><span className='badge bg-danger position-absolute d-flex align-items-center text-center'>{movie.vote_average}</span></h4></div>
+                        
+                        </div>
                         <div className='card-body'>
-                            <h5 className='card-title'>{movie.name}</h5>
-                            <p className='card-text'>{movie.overview}</p>
+                            <h5 className='card-title'>{movie.title}</h5>
+                            <p className='card-text'>{movie.textContent}</p>
                             <div className='d-flex justify-content-between alighn-items-center'>
-                                <button onClick={(event) => props.deleteMovieProp(movie)} className='btn btn-md btn-outline-danger'>Delete</button>
-                                <h2><span className='badge bg-info'>{movie.rating}</span></h2>
+                                <button onClick={(event) => props.deleteMovieProp(movie)} className='btn btn-md btn-outline-danger d-none'>Delete</button>
                             </div>
                         </div>
                     </div>
